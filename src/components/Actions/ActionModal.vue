fd591b9992199b48d6934ddc519e555e4b9a4291
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-04 19:36:15 +0430

    set Select to none for modal

--- src/components/Actions/ActionModal.vue
+++ src/components/Actions/ActionModal.vue
@@ -3,7 +3,7 @@
 
     <div class="fixed inset-0 bg-black opacity-30 z-10"></div>
     <div
-      class="fixed bg-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-10/12 max-w-md max-h-[50vh] overflow-x-hidden overflow-y-auto z-[11] rounded-md pt-8 pb-4 px-4">
+      class="fixed bg-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-10/12 max-w-md max-h-[50vh] overflow-x-hidden overflow-y-auto z-[11] rounded-md pt-8 pb-4 px-4 select-none">
       <button class="absolute top-1 right-1 bg-red-400 w-6 h-6 rounded-md align-middle text-white" @click="emits('close')">&#x2715;</button>
       <div v-html="props.text"></div>
     </div>

7727b549ba9a9a1a6ef49c50b75f271483d2e1b2
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-04 18:08:06 +0430

    add placeholder for loading data in dashboard

---
+++ src/components/Actions/PlaceHolderActions.vue
@@ -0,0 +1,11 @@
+<template>
+    <ActionsLayout>
+        <div class="aspect-square w-full animate-pulse rounded-md shadow bg-gray-50" v-for="x in 8" :key="x">
+
+        </div>
+    </ActionsLayout>
+</template>
+
+<script setup>
+import ActionsLayout from './ActionsLayout.vue';
+</script>

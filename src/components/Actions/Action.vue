2c43bb767c576ec40289ad26cc6cea5efbe3f181
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-04 19:40:48 +0430

    increase height of card content in Action.vue

--- src/components/Actions/Action.vue
+++ src/components/Actions/Action.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative w-full overflow-hidden rounded-md shadow">
     <img class="object-cover block w-full aspect-square cursor-pointer z-0" :src="img" @click="emits('imgClicked')">
-    <div class="flex flex-row justify-start items-center gap-1 py-2.5 px-3 absolute bottom-0 right-0 left-0 z-10 bg-content shadow-[0_-2px_8px_0_rgba(0,0,0,0.1)]">
+    <div class="flex flex-row justify-start items-center gap-1 p-3 absolute bottom-0 right-0 left-0 z-10 bg-content shadow-[0_-2px_8px_0_rgba(0,0,0,0.1)]">
       <h4 class="grow overflow-hidden whitespace-nowrap text-ellipsis">{{ props.title }}</h4>
       <span class="flex flex-row justify-end items-center gap-1">
         <TrashIcon class="h-5 w-5 text-gray-700 cursor-pointer" @click="emits('delete')" />

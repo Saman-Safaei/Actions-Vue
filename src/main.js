a5b064be353369ae645b1d50c06a29e7f99aa9f6
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-06 20:39:30 +0430

    create i18n instance with composition api option

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createI18n } from "vue-i18n";
 
 import App from './App.vue';
 import router from './router';
 import Validators from './plugins/vee_rules';
+import i18n from "./i18n";
 
 import "./assets/tailwind.scss";
 
 const app = createApp(App);
 
+app.use(createI18n({ legacy: false, locale: "fa", fallbackLocale: "en", messages: i18n }));
 app.use(createPinia());
 app.use(router);
 app.use(Validators);

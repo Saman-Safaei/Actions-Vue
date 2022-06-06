b87301c5be408281de8f47e7b39fb203ff73d125
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-06 20:38:35 +0430

    nothing changed in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
-<script setup>
-import NotificationLayout from './components/Notification/NotificationLayout.vue';
-</script>
-
 <template>
   <NotificationLayout />
   <main class="max-w-5xl mx-auto px-5 py-1">
     <RouterView />
   </main>
 </template>
+
+<script setup>
+import NotificationLayout from './components/Notification/NotificationLayout.vue';
+import { useI18n } from "vue-i18n";
+</script>

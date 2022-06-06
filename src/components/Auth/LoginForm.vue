e0f14f8ef73d3b189b28fc09651aa5cc2564b6a9
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-07 02:11:57 +0430

    add right padding to inputs

--- src/components/Auth/LoginForm.vue
+++ src/components/Auth/LoginForm.vue
@@ -2,11 +2,11 @@
   <div :dir="direction" class="bg-white rounded-md shadow w-full sm:w-3/5 md:w-2/5 py-6 px-5 select-none">
     <h3 class="text-center font-bold text-2xl mb-4 text-teal-800">Actions</h3>
     <form dir="ltr" @submit.prevent="onSubmit" novalidate>
-      <div class="flex flex-row flex-nowrap items-stretch gap-2 bg-gray-200 py-2 px-2 rounded-md mb-2">
+      <div class="flex flex-row flex-nowrap items-stretch gap-2 bg-gray-200 py-2 pl-2 pr-4 rounded-md mb-2">
         <UserCircleIcon class="w-8 h-8 text-gray-400" />
         <input :dir="direction" class="grow" type="text" :placeholder="t('texts.username')" autocomplete="off" v-model="username">
       </div>
-      <div class="flex flex-row flex-nowrap items-stretch gap-2 bg-gray-200 py-2 px-2 rounded-md mb-4">
+      <div class="flex flex-row flex-nowrap items-stretch gap-2 bg-gray-200 py-2 pl-2 pr-4 rounded-md mb-4">
         <LockClosedIcon class="w-8 h-8 text-gray-400" />
         <input :dir="direction" class="grow" type="text" :placeholder="t('texts.password')" autocomplete="off" v-model="password">
       </div>

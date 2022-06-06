2f04b8a56ab7332e3a643e5a25f45c949d4e385a
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-06 20:38:56 +0430

    create messages in i18n

---
+++ src/i18n/index.js
@@ -0,0 +1,18 @@
+export default {
+  en: {
+    buttons: {
+      new: "New",
+      logout: "Logout",
+      create: "Create",
+      cancel: "Cancel"
+    }
+  },
+  fa: {
+    buttons: {
+      new: "جدید",
+      logout: "خروج از حساب",
+      create: "ساختن",
+      cancel: "لغو"
+    }
+  }
+}

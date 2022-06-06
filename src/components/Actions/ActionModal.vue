97917e3028d07f4406220b027b3b9ef1c59f3c53
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-06 23:25:59 +0430

    center the text and add persian text

--- src/components/Actions/ActionModal.vue
+++ src/components/Actions/ActionModal.vue
@@ -2,15 +2,20 @@
   <Teleport to="body">
 
     <div class="fixed inset-0 bg-black opacity-30 z-10"></div>
-    <div
+    <div :class="center"
       class="fixed bg-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-10/12 max-w-md max-h-[50vh] overflow-x-hidden overflow-y-auto z-[11] rounded-md pt-8 pb-4 px-4 select-none">
-      <button class="absolute top-1 right-1 bg-red-400 w-6 h-6 rounded-md align-middle text-white" @click="emits('close')">&#x2715;</button>
+      <button class="absolute top-1 right-1 bg-red-400 w-6 h-6 rounded-md align-middle text-white"
+        @click="emits('close')">&#x2715;</button>
       <div v-html="props.text"></div>
     </div>
   </Teleport>
 </template>
 
 <script setup>
-const props = defineProps(["text"]);
+import { computed } from 'vue';
+
+const props = defineProps(["text", "center"]);
 const emits = defineEmits(["close"]);
+
+const center = computed(() => ({ "text-center": props.center }));
 </script>

fa36c0083da22fa0bac0da1d228b1047e764aa80
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-15 15:12:38 +0430

    add some translated text to i18n

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -6,7 +6,7 @@ export default {
       create: "Create",
       cancel: "Cancel",
       login: "Login",
-      signup: "SignUp",
+      signup: "Signup",
       wait: "Please wait ..."
     },
     texts: {
@@ -17,7 +17,11 @@ export default {
       password: "Password",
       about: "About this site",
       noAction: "There is no action to show",
-      createAction: "Click on \"New\" button on navbar to create an action."
+      createAction: "Click on \"New\" button on navbar to create an action",
+      title: "Title",
+      chooseFile: "Click here to choose an image",
+      forgot: "Forgot Password",
+      remember: "Remember me"
     }
   },
   fa: {
@@ -38,7 +42,11 @@ export default {
       password: "رمز عبور",
       about: "درباره این سایت",
       noAction: "هیچ اکشنی برای نمایش نیست",
-      createAction: "با زدن دکمه \"جدید\" میتونین یک اکشن جدید درست کنین"
+      createAction: "با زدن دکمه \"جدید\" میتونین یک اکشن جدید درست کنین",
+      title: "عنوان",
+      chooseFile: "برای انتخاب عکس کلیک کنید",
+      forgot: "فراموشی رمز",
+      remember: "مرا به خاطر بسپار"
     }
   }
 }

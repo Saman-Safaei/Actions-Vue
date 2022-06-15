415d333cd97181a983ef1cb8cce922d6e038a67a
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-15 15:10:51 +0430

    remove action model and add composables instead

---
+++ src/composables/createAction.js
@@ -0,0 +1,38 @@
+import { ref, reactive, isRef, unref } from "vue";
+import api from "../api";
+
+const endpoint = '/actions';
+
+export function useCreateAction(img, title, body, notifStore) {
+  const token = localStorage.getItem("token");
+
+  const pending = ref(false);
+
+  function post(cb, fcb) {
+    pending.value = true;
+
+    const imgVal = unref(img);
+    const titleVal = unref(title);
+    const bodyVal = unref(body);
+
+
+    const formData = new FormData();
+    formData.append("avatar", imgVal);
+    formData.append("title", titleVal);
+    formData.append("body", bodyVal);
+
+    api.post(endpoint, formData, { headers: { "Content-Type": "multipart/form-data", token } })
+      .then(res => {
+        notifStore.pushNotification("Action Created");
+        pending.value = false;
+        cb(res);
+      })
+      .catch(err => {
+        notifStore.pushNotification("An error occurred");
+        pending.value = false;
+        fcb(err);
+      });
+  }
+
+  return {post, pending};
+}
--- src/composables/getActions.js
+++ src/composables/getActions.js
@@ -1,19 +1,22 @@
 import { ref, reactive } from "vue";
+import { useUserStore } from "../stores/users";
 import api from "../api";
 
 const endpoint = '/actions';
 
 export function useAllActions(router, userStore) {
-  const token = localStorage.getItem("token"); // Get Token From LocalStorage
+  const token = useUserStore().token; // Get Token From LocalStorage
 
   const allActions = reactive([]);
   const error = reactive({});
   const loaded = ref(false);
 
+  load();
+
   function load() {
     loaded.value = false;
-    
-    api.get(endpoint, { headers: { token } })
+
+    api.get(endpoint, { headers: { token: token } })
       .then(res => {
         allActions.length = 0;
         allActions.push(...res.data);
@@ -32,7 +35,6 @@ export function useAllActions(router, userStore) {
       });
   }
 
-  load();
 
   return { data: allActions, error: error, loaded: loaded, load: load };
 }

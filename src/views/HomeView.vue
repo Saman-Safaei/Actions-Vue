3e0e5d67a642fbdf985770d6c63e73783b6aba9d
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-17 20:36:42 +0430

    create and add basic codes

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <HomeNavbar />
+    <div>
+      <img src="/images/i_images.svg" alt="">
+    </div>
+  </header>
+</template>
+
+<script setup>
+import HomeNavbar from '../components/Navigation/HomeNavbar.vue';
+</script>

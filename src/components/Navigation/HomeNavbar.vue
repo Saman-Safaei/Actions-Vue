c03ad07704ac5359d0665e372fd465c700a35025
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-22 23:29:27 +0430

    add logo to navbar

--- src/components/Navigation/HomeNavbar.vue
+++ src/components/Navigation/HomeNavbar.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="flex flex-row justify-start items-stretch h-20 bg-teal-300">
-    <div class=""></div>
+  <div class="flex flex-row justify-start items-stretch h-16 bg-white/80 backdrop-blur-sm z-[8] px-8">
+    <div class="h-full py-4">
+      <router-link to="/"><img class="h-full" src="../../assets/logo.svg" alt="logo"></router-link>
+    </div>
   </div>
 </template>
 

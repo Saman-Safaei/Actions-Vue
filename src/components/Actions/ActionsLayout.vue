901cd3cbf1990ba3b78187a196128b36ca16ab83
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-22 23:27:15 +0430

    add lg brickpoint ( 5 cols in large screens )

--- src/components/Actions/ActionsLayout.vue
+++ src/components/Actions/ActionsLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 p-3">
+  <div class="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 p-3">
     <slot></slot>
   </div>
 </template>

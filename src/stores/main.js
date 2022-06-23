c763f3745eec2916b5cdf31a81cdced634749551
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-23 13:09:50 +0430

    add toggle and a bool for mobile home modal

--- src/stores/main.js
+++ src/stores/main.js
@@ -3,10 +3,14 @@ import { ref } from "vue";
 
 export const useMainStore = defineStore("main", () => {
   const showCreateModal = ref(false);
+  const showHomeDrawer = ref(false);
 
   function toggleModal() {
     showCreateModal.value = !showCreateModal.value;
   }
+  function toggleHomeDrawer() {
+    showHomeDrawer.value = !showHomeDrawer.value;
+  }
 
-  return { showCreateModal, toggleModal };
+  return { showCreateModal, toggleModal, showHomeDrawer, toggleHomeDrawer };
 });

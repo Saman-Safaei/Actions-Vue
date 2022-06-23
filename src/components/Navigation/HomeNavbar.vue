654a21685ad28bd3bcd25bc47634f69828613c99
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-23 13:08:34 +0430

    create a beatiful home navbar

--- src/components/Navigation/HomeNavbar.vue
+++ src/components/Navigation/HomeNavbar.vue
@@ -1,11 +1,41 @@
 <template>
-  <div class="flex flex-row justify-start items-stretch h-16 bg-white/80 backdrop-blur-sm z-[8] px-8">
-    <div class="h-full py-4">
-      <router-link to="/"><img class="h-full" src="../../assets/logo.svg" alt="logo"></router-link>
+  <div class="flex flex-row justify-start items-stretch h-16 bg-white backdrop-blur-sm z-[8] px-3">
+    <div class="py-4 flex flex-row items-center gap-2">
+      <router-link :to="{ name: 'home' }" class="h-full"><img class="h-full" src="../../assets/logo.svg" alt="logo"></router-link>
+    </div>
+    <div class="flex-grow"></div>
+    <nav class="hidden md:flex flex-row items-center">
+      <ul class="flex flex-row items-center h-full gap-3">
+        <li>Preview</li>
+        <li>Features</li>
+        <li>About us</li>
+      </ul>
+    </nav>
+    <div class="hidden md:block md:flex-grow"></div>
+    <div class="hidden md:flex flex-row gap-2 items-center">
+      <button class="py-1.5 px-2.5 rounded-md transition"
+        @click="userStore.logOut()" v-if="userStore.loggingIn">Log out</button>
+      <router-link class="bg-teal-600 hover:bg-teal-500 text-white py-1.5 px-2 rounded-md transition"
+        :to="{ name: 'auth' }">{{ signinButtonName }}</router-link>
+    </div>
+
+    <div class="md:hidden flex flex-row items-center">
+      <button @click="mainStore.toggleHomeDrawer()"><DotsVerticalIcon class="w-6 h-6 black"/></button>
     </div>
   </div>
 </template>
 
 <script setup>
+import { computed } from "vue";
 
+import { DotsVerticalIcon } from "@heroicons/vue/outline";
+import { useUserStore } from "../../stores/users";
+import { useMainStore } from "../../stores/main";
+
+const userStore = useUserStore();
+const mainStore = useMainStore();
+
+const signinButtonName = computed(() => {
+  return (userStore.loggingIn) ? "Dashboard" : "Log in";
+});
 </script>

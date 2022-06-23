016bae169f7124a0b1e197c5de5967e658c56fde
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-23 13:10:16 +0430

    remove dummy text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <template>
   <header>
     <HomeNavbar />
+    <HomeDrawer />
     <div class="bg-white p-8">
       <div class="flex flex-col md:flex-row items-stretch gap-x-16 gap-y-8 max-w-6xl mx-auto">
         <div class="flex flex-col order-2 md:order-1 gap-3 justify-center items-center md:items-start flex-grow">
           <h1
-            class="text-4xl md:text-5xl font-bold text-transparent text-center md:text-start bg-gradient-to-r from-teal-500 via-teal-400 to-teal-500 bg-clip-text">
+            class="text-4xl md:text-5xl font-bold text-transparent text-center md:text-start bg-gradient-to-r from-teal-500 to-teal-400 bg-clip-text">
             Actions Vue</h1>
           <p class="text-lg text-center text-gray-600 md:text-start">Lorem ipsum dolor sit amet consectetur adipisicing
             elit. Magni eveniet quo repellendus. Nisi quos voluptas maxime ab doloremque nam officiis qui aspernatur
@@ -20,10 +21,11 @@
     </div>
   </header>
   <main class="max-w-6xl mx-auto">
-    <div>can you see the dark ? can you fix a broken soul . can you feel my heart ... no you can't feel my heart . but I create app with a effienche aaa ! its a simple joke with your broken board</div>
+    
   </main>
 </template>
 
 <script setup>
 import HomeNavbar from '../components/Navigation/HomeNavbar.vue';
+import HomeDrawer from '../components/Navigation/HomeDrawer.vue';
 </script>

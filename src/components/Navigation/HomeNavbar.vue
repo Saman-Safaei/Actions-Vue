1a282f6d10eacc26427578cb81c14b5cc8e2d982
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-07-08 16:51:38 +0430

    add animaiton to navbar and remove slider from homepage

--- src/components/Navigation/HomeNavbar.vue
+++ src/components/Navigation/HomeNavbar.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="flex flex-row justify-start items-stretch h-16 bg-white backdrop-blur-sm z-[8] px-3">
+  <div
+    class="flex flex-row justify-start items-stretch h-16 bg-white backdrop-blur-sm z-[8] px-3"
+    v-bind="$attrs"
+  >
     <div class="py-4 flex flex-row items-center gap-2">
-      <router-link :to="{ name: 'home' }" class="h-full"><img class="h-full" src="../../assets/logo.svg" alt="logo"></router-link>
+      <router-link :to="{ name: 'home' }" class="h-full"
+        ><img class="h-full" src="../../assets/logo.svg" alt="logo"
+      /></router-link>
     </div>
     <div class="flex-grow"></div>
     <nav class="hidden md:flex flex-row items-center">
@@ -13,14 +18,24 @@
     </nav>
     <div class="hidden md:block md:flex-grow"></div>
     <div class="hidden md:flex flex-row gap-2 items-center">
-      <button class="py-1.5 px-2.5 rounded-md transition"
-        @click="userStore.logOut()" v-if="userStore.loggingIn">Log out</button>
-      <router-link class="bg-teal-600 hover:bg-teal-500 text-white py-1.5 px-2 rounded-md transition"
-        :to="{ name: 'auth' }">{{ signinButtonName }}</router-link>
+      <button
+        class="py-1.5 px-2.5 rounded-md transition"
+        @click="userStore.logOut()"
+        v-if="userStore.loggingIn"
+      >
+        Log out
+      </button>
+      <router-link
+        class="bg-teal-600 hover:bg-teal-500 text-white py-1.5 px-2 rounded-md transition"
+        :to="{ name: 'auth' }"
+        >{{ signinButtonName }}</router-link
+      >
     </div>
 
     <div class="md:hidden flex flex-row items-center">
-      <button @click="mainStore.toggleHomeDrawer()"><DotsVerticalIcon class="w-7 h-7 black"/></button>
+      <button @click="mainStore.toggleHomeDrawer()">
+        <DotsVerticalIcon class="w-7 h-7 black" />
+      </button>
     </div>
   </div>
 </template>
@@ -35,5 +50,7 @@ import { useMainStore } from "../../stores/main";
 const userStore = useUserStore();
 const mainStore = useMainStore();
 
-const signinButtonName = computed(() => (userStore.loggingIn) ? "Dashboard" : "Log in");
+const signinButtonName = computed(() =>
+  userStore.loggingIn ? "Dashboard" : "Log in"
+);
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,43 +1,128 @@
 <template>
   <header>
-    <HomeNavbar />
+    <HomeNavbar :class="navbarStyles" />
+    <div class="h-16" v-show="navbarFixed"></div>
     <HomeDrawer />
     <div class="bg-white p-8">
-      <div class="flex flex-col md:flex-row items-stretch gap-x-16 gap-y-8 max-w-6xl mx-auto">
-        <div class="flex flex-col order-2 md:order-1 gap-3 justify-center items-center md:items-start flex-grow">
+      <div
+        class="flex flex-col md:flex-row items-stretch gap-x-16 gap-y-8 max-w-6xl mx-auto"
+      >
+        <div
+          class="flex flex-col order-2 md:order-1 gap-3 justify-center items-center md:items-start flex-grow"
+        >
           <h1
-            class="text-4xl md:text-5xl font-bold text-transparent text-center md:text-start bg-gradient-to-r from-teal-500 to-teal-400 bg-clip-text">
-            Actions Vue</h1>
-          <p class="text-lg text-center text-gray-600 md:text-start">Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Magni eveniet quo repellendus. Nisi quos voluptas maxime ab doloremque nam officiis qui aspernatur
-            eius soluta natus quae, reprehenderit aperiam, praesentium unde.</p>
+            class="text-4xl md:text-5xl font-bold text-transparent text-center md:text-start bg-gradient-to-r from-teal-500 to-teal-400 bg-clip-text"
+          >
+            Actions Vue
+          </h1>
+          <p class="text-lg text-center text-gray-600 md:text-start">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+            eveniet quo repellendus. Nisi quos voluptas maxime ab doloremque nam
+            officiis qui aspernatur eius soluta natus quae, reprehenderit
+            aperiam, praesentium unde.
+          </p>
           <button
-            class="mt-4 bg-teal-600 hover:bg-teal-500 active:bg-teal-700 text-white px-4 py-2.5 rounded-full transition-all">Manage
-            your
-            notes</button>
+            class="mt-4 bg-teal-600 hover:bg-teal-500 active:bg-teal-700 text-white px-4 py-2.5 rounded-full transition-all"
+          >
+            Manage your notes
+          </button>
         </div>
-        <img class="w-10/12 mx-auto md:h-80 md:w-auto drop-shadow-lg order-1 md:order-2" src="/images/i_images.svg"
-          alt="logo">
+        <img
+          class="w-10/12 mx-auto md:h-80 md:w-auto drop-shadow-lg order-1 md:order-2"
+          src="/images/i_images.svg"
+          alt="logo"
+        />
       </div>
-
     </div>
   </header>
   <main>
-    <section id="preview" class="bg-slate-300">
-
-      <Swiper :slides-per-view="'auto'" :centered-slides="true" :space-between="50" :effect="'coverflow'">
-        <swiper-slide><span>this is good</span></swiper-slide>
-        <swiper-slide><span>thiis is not good</span></swiper-slide>
-        <swiper-slide><span>this is bad</span></swiper-slide>
-      </Swiper>
-
+    <section
+      id="features"
+      class="px-4 py-8 flex flex-col items-stretch gap-10 md:gap-14"
+    >
+      <h3 class="text-center text-2xl md:text-4xl text-gray-600">
+        Features of <span class="text-teal-600">Action-Vue</span>
+      </h3>
+      <div
+        class="flex flex-col md:flex-row items-stretch gap-4 max-w-6xl mx-auto"
+      >
+        <img class="md:max-w-md" src="/images/preview.png" alt="preview" />
+        <div class="flex flex-col gap-2 grow py-4">
+          <h3 class="text-xl font-bold">Security</h3>
+          <p class="text-lg text-gray-700">
+            Your can easily upload your images with a simple note in our cloud
+            database. Your images are secure and not accessible for another
+            people.
+          </p>
+        </div>
+      </div>
+      <div
+        class="flex flex-col md:flex-row-reverse items-stretch gap-4 max-w-6xl mx-auto"
+      >
+        <img class="md:max-w-md" src="/images/preview.png" alt="preview" />
+        <div class="flex flex-col gap-2 grow py-4">
+          <h3 class="text-xl font-bold">Easy to use</h3>
+          <p class="text-lg text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+        </div>
+      </div>
+      <div
+        class="flex flex-col md:flex-row items-stretch gap-4 max-w-6xl mx-auto"
+      >
+        <img class="md:max-w-md" src="/images/preview.png" alt="preview" />
+        <div class="flex flex-col gap-2 grow py-4">
+          <h3 class="text-xl font-bold">Fast</h3>
+          <p class="text-lg text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
     </section>
   </main>
 </template>
 
 <script setup>
-import HomeNavbar from '../components/Navigation/HomeNavbar.vue';
-import HomeDrawer from '../components/Navigation/HomeDrawer.vue';
+import HomeNavbar from "../components/Navigation/HomeNavbar.vue";
+import HomeDrawer from "../components/Navigation/HomeDrawer.vue";
+import { computed, onMounted, ref } from "vue";
 
-import { Swiper, SwiperSlide} from "swiper/vue"
+const navbarFixed = ref(false);
+const navbarStyles = computed(() =>
+  navbarFixed.value
+    ? {
+        fixed: true,
+        "top-0": true,
+        "left-0": true,
+        "right-0": true,
+        "bg-opacity-70": true,
+        "shadow-md": true,
+        "slide-top": true,
+      }
+    : {}
+);
+
+onMounted(() => {
+  document.addEventListener("scroll", onScrollPage);
+
+  function onScrollPage() {
+    if (window.scrollY > 200 && !navbarFixed.value) {
+      navbarFixed.value = true;
+    }
+    if (window.scrollY <= 200 && navbarFixed.value) {
+      navbarFixed.value = false;
+    }
+  }
+});
 </script>

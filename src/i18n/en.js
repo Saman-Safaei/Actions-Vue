c0ddd3469fc59247c4719edd54645c561fec35ca
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-07-08 21:39:29 +0430

    add new button texts

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,25 +1,28 @@
 export default {
-    buttons: {
-      new: "New",
-      logout: "Logout",
-      create: "Create",
-      cancel: "Cancel",
-      login: "Login",
-      signup: "Signup",
-      wait: "Please wait ..."
-    },
-    texts: {
-      haveAccount: "Have an account ?",
-      dhaveAccount: "Don't have an account ?",
-      username: "Username",
-      email: "Email Address",
-      password: "Password",
-      about: "About this site",
-      noAction: "There is no action to show",
-      createAction: "Click on \"New\" button on navbar to create an action",
-      title: "Title",
-      chooseFile: "Click here to choose an image",
-      forgot: "Forgot Password",
-      remember: "Remember me"
-    }
-}
+  buttons: {
+    new: "New",
+    logout: "Logout",
+    create: "Create",
+    cancel: "Cancel",
+    login: "Login",
+    signup: "Signup",
+    wait: "Please wait ...",
+    preview: "Preview",
+    features: "Features",
+    about: "About us",
+  },
+  texts: {
+    haveAccount: "Have an account ?",
+    dhaveAccount: "Don't have an account ?",
+    username: "Username",
+    email: "Email Address",
+    password: "Password",
+    about: "About this site",
+    noAction: "There is no action to show",
+    createAction: 'Click on "New" button on navbar to create an action',
+    title: "Title",
+    chooseFile: "Click here to choose an image",
+    forgot: "Forgot Password",
+    remember: "Remember me",
+  },
+};

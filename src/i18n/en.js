31fcbca7448eb3f3a1387145e8d9f84515a44c41
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-07-11 17:36:59 +0430

    add some translations

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -35,6 +35,7 @@ export default {
     fastText:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     featuresTitle: 'Features of',
+    previewTitle: 'Preview of',
     headerText:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eveniet quo repellendus. Nisi quos voluptas maxime ab doloremque nam officiis qui aspernatur eius soluta natus quae, reprehenderit aperiam, praesentium unde.',
   },

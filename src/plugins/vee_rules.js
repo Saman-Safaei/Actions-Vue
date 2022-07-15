f861982850b1c631beedf13587e620419b54edc6
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-07-15 21:19:03 +0430

    generate translated messages

--- src/plugins/vee_rules.js
+++ src/plugins/vee_rules.js
@@ -1,24 +1,34 @@
-import { defineRule, configure } from "vee-validate";
-import { min, email, required } from "@vee-validate/rules";
-
+import { defineRule, configure } from 'vee-validate';
+import { min, email, required } from '@vee-validate/rules';
+import i18n from '../i18n';
 
 export default {
-    install() {
-        function messages (context) {
-            return {
-                required: `The ${context.field} field is required.`,
-                min: `Minimum length of the ${context.field} field is ${context.rule.params[0]}`,
-                email: `Enter a valid email.`
-            }
-        }
-        defineRule("required", required);
-        defineRule("min", min);
-        defineRule("email", email);
+  install() {
+    const { locale, t } = i18n.global;
 
-        configure({
-            generateMessage: context => {
-                return messages(context)[context.rule.name];
-            }
-        });
+    function messages(context) {
+      return {
+        fa: {
+          required: `فیلد ${t('texts.' + context.field)} اجباری است.`,
+          min: `تعداد کاراکتر های وارد شده در ${context.field} کمتر از ${context.rule.params[0]} تا است.`,
+          email: `ایمیل وارد شده معتبر نیست‍‍‍`,
+        },
+        en: {
+          required: `The ${context.field} field is required.`,
+          min: `Minimum length of the ${context.field} field is ${context.rule.params[0]}`,
+          email: `Enter a valid email.`,
+        },
+      };
     }
-}
+
+    defineRule('required', required);
+    defineRule('min', min);
+    defineRule('email', email);
+
+    configure({
+      generateMessage: context => {
+        return messages(context)[locale.value][context.rule.name];
+      },
+    });
+  },
+};

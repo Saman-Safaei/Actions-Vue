ff245a4a9d88f6dd8a0a039225f581c2f86869a1
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-07-15 21:39:52 +0430

    new actions modal

--- src/components/Actions/ActionModal.vue
+++ src/components/Actions/ActionModal.vue
@@ -1,11 +1,14 @@
 <template>
   <Teleport to="body">
-
     <div class="fixed inset-0 bg-black opacity-30 z-10"></div>
-    <div :class="center"
+    <div
+      :class="center"
       class="fixed bg-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-10/12 max-w-md max-h-[50vh] overflow-x-hidden overflow-y-auto z-[11] rounded-md pt-8 pb-4 px-4 select-none">
-      <button class="absolute top-1 right-1 bg-red-400 w-6 h-6 rounded-md align-middle text-center text-white"
-        @click="emits('close')">&#x2715;</button>
+      <button
+        class="absolute top-1 right-1 bg-red-400 w-6 h-6 rounded-md align-middle text-center text-white"
+        @click="emits('close')">
+        &#x2715;
+      </button>
       <div v-html="props.text"></div>
     </div>
   </Teleport>
@@ -14,8 +17,9 @@
 <script setup>
 import { computed } from 'vue';
 
-const props = defineProps(["text", "center"]);
-const emits = defineEmits(["close"]);
+const props = defineProps(['text', 'center']);
+const emits = defineEmits(['close']);
 
-const center = computed(() => ({ "text-center": props.center }));
+const center = computed(() => ({ 'text-center': props.center }));
+console.log(props.text);
 </script>

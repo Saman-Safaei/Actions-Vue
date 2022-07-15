485b0eaf7da43cfa4e98314fb1e408d9f8d0541d
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-07-15 21:19:17 +0430

    use direction for language

--- src/components/Notification/Notification.vue
+++ src/components/Notification/Notification.vue
@@ -1,37 +1,20 @@
 <template>
-  <div class="flex flex-row flex-nowrap items-center gap-2 px-3 py-4 mb-2 bg-gradient-to-tr from-teal-500 to-teal-600 rounded-l-lg">
-    <button @click="store.removeNotif(props.nId)" class="text-lime-200 shrink">x</button>
-    <p class="text-center text-lime-50 grow m-0"><slot></slot></p>
+  <div
+    class="flex flex-row flex-nowrap items-center gap-2 px-3 py-4 mb-2 bg-gradient-to-tr from-teal-500 to-teal-600 rounded-l-lg">
+    <button @click="store.removeNotif(props.nId)" class="text-lime-200 shrink">
+      x
+    </button>
+    <p :dir="direction" class="text-center text-lime-50 grow m-0">
+      <slot></slot>
+    </p>
   </div>
 </template>
 
 <script setup>
 import { useNotifStore } from '../../stores/notification';
+import { useLocale } from '../../composables/locale';
 
+const { direction } = useLocale();
 const store = useNotifStore();
-const props = defineProps(["nId"]);
+const props = defineProps(['nId']);
 </script>
-
-<style lang="scss" scoped>
-
-.notification {
-  // display: flex;
-  // padding: 1rem 0.6rem;
-  // margin-bottom: 0.5rem;
-  // background: linear-gradient(6deg, $dark, $dark-green);
-  // border-radius: 0.5rem 0 0 0.5rem;
-  // box-shadow: $light-green 0 2px 5px ;
-  // flex-direction: row;
-  // flex-wrap: nowrap;
-
-  .notification__remove {
-    // color: $light-green;
-    flex-shrink: 1;
-  }
-  .notification__content {
-    text-align: center;
-    // color: $light;
-    flex-grow: 1;
-  }
-}
-</style>

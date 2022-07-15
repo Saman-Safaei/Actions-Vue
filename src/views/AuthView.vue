4646d1eb81b98afb8144c815e4710ae9268ed420
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-07-15 21:19:26 +0430

    make background brighter

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,19 +1,19 @@
 <template>
   <div
-    class="flex flex-col px-2 min-h-[90vh] md:min-h-[98vh] items-center justify-center"
-  >
+    class="flex flex-col px-2 min-h-[90vh] md:min-h-[98vh] items-center justify-center">
     <Transition name="auth-fade" mode="out-in">
       <component :is="component" @switch="switchComponent" />
     </Transition>
     <div
       :dir="direction"
-      class="flex flex-row justify-between w-full max-w-sm py-2 px-3"
-    >
-      <router-link :to="{ name: 'about' }" class="text-teal-900">{{
-        t("texts.about")
+      class="flex flex-row justify-between items-baseline w-full max-w-sm py-2 px-3">
+      <router-link :to="{ name: 'home' }" class="text-teal-900">{{
+        t('buttons.home')
       }}</router-link>
       <span class="flex flex-row gap-2">
-        <button @click="switchLang" class="text-teal-800">{{ locale }}</button>
+        <button @click="switchLang" class="text-teal-800">
+          {{ locale.toUpperCase() }}
+        </button>
         <GlobeIcon class="text-gray-500 h-5 w-5" />
       </span>
     </div>
@@ -22,21 +22,19 @@
     :center="true"
     v-if="showMessage"
     text="Make sure of your enabled VPN<br>مطمئن شوید به فیلتر شکن متصل اید"
-    @close="closeMessage"
-  />
+    @close="closeMessage" />
   <div
-    class="fixed inset-0 bg-gradient-to-tr from-teal-100 to-teal-200 -z-20"
-  ></div>
+    class="fixed inset-0 bg-gradient-to-tr from-teal-300 to-teal-100 -z-20"></div>
 </template>
 
 <script setup>
-import SignUp from "../components/Auth/SignUp.vue";
-import Login from "../components/Auth/LoginForm.vue";
-import ActionModal from "../components/Actions/ActionModal.vue";
+import SignUp from '../components/Auth/SignUp.vue';
+import Login from '../components/Auth/LoginForm.vue';
+import ActionModal from '../components/Actions/ActionModal.vue';
 
-import { ref, computed } from "vue";
-import { GlobeIcon } from "@heroicons/vue/outline";
-import { useLocale } from "../composables/locale";
+import { ref, computed } from 'vue';
+import { GlobeIcon } from '@heroicons/vue/outline';
+import { useLocale } from '../composables/locale';
 
 const showLogin = ref(true);
 const showMessage = ref(true);

1e93747f0033e7ddd95db084759b062821a48381
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-07-15 21:36:38 +0430

    make language text uppercase

--- src/components/Navigation/Navbar.vue
+++ src/components/Navigation/Navbar.vue
@@ -1,42 +1,49 @@
 <template>
-  <div :dir="direction" class="flex flex-row items-center gap-1.5 p-3 shadow-md rounded-md bg-white">
-  
-    <OutlinedButton @click="mainStore.toggleModal">{{ t("buttons.new") }}</OutlinedButton>
-    <OutlinedButton @click="logoutAndPush">{{ t("buttons.logout") }}</OutlinedButton>
+  <div
+    :dir="direction"
+    class="flex flex-row items-center gap-1.5 p-3 shadow-md rounded-md bg-white">
+    <OutlinedButton @click="mainStore.toggleModal">{{
+      t('buttons.new')
+    }}</OutlinedButton>
+    <OutlinedButton @click="logoutAndPush">{{
+      t('buttons.logout')
+    }}</OutlinedButton>
 
     <span class="grow"></span>
 
-    <button class="border-solid border-b-4 border-teal-600" @click="changeLang">{{ locale }}</button>
-    <router-link :to="{ name: 'home' }"><img class="w-8 h-8" src="@/assets/logo.svg" alt="brand"></router-link>
-
+    <button class="border-solid border-b-4 border-teal-600" @click="changeLang">
+      {{ locale.toUpperCase() }}
+    </button>
+    <router-link :to="{ name: 'home' }"
+      ><img class="w-8 h-8" src="@/assets/logo.svg" alt="brand"
+    /></router-link>
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
-import { useI18n } from "vue-i18n";
-import { useRouter } from "vue-router";
-import { useUserStore } from "../../stores/users";
-import { useMainStore } from "../../stores/main";
-import OutlinedButton from "../Buttons/OutlinedButton.vue";
-
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { useRouter } from 'vue-router';
+import { useUserStore } from '../../stores/users';
+import { useMainStore } from '../../stores/main';
+import OutlinedButton from '../Buttons/OutlinedButton.vue';
 
 const mainStore = useMainStore();
 const userStore = useUserStore();
 const router = useRouter();
 
-const { t, locale } = useI18n({ useScope: "global" });
+const { t, locale } = useI18n({ useScope: 'global' });
 
 function logoutAndPush() {
   userStore.logOut();
-  router.push({ name: "auth" });
+  router.push({ name: 'auth' });
 }
 function changeLang() {
-  locale.value === "en" ? locale.value = 'fa' : locale.value = 'en';
+  locale.value === 'en' ? (locale.value = 'fa') : (locale.value = 'en');
 }
 
 const direction = computed(() => {
-  return (locale.value === "fa") ? "rtl" : "ltr";
+  return locale.value === 'fa' ? 'rtl' : 'ltr';
 });
 </script>
 
@@ -64,7 +71,6 @@ const direction = computed(() => {
 //     flex-grow: 1;
 //   }
 
-
 //   .navbar__item--btn {
 //     background: $dark-green;
 //     padding: $p-xs2 $p-xs;
@@ -81,7 +87,6 @@ const direction = computed(() => {
 //       background: $dark;
 //     }
 //   }
-
 
 // }
 </style>

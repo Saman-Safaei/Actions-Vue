5cae7f4507aab225442522b7737a576d51498559
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-07-18 15:01:52 +0430

    create a new modal and replace it with the old modal

--- src/components/Actions/ActionModal.vue
+++ src/components/Actions/ActionModal.vue
@@ -1,28 +1,38 @@
 <template>
   <Teleport to="body">
-    <div class="fixed inset-0 bg-black opacity-30 z-10"></div>
     <div
-      :class="center"
-      class="fixed bg-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-10/12 max-w-md max-h-[50vh] overflow-x-hidden overflow-y-auto z-[11] rounded-md pt-8 pb-4 px-4 select-none">
-      <button
-        class="absolute top-1 right-1 bg-red-400 w-6 h-6 rounded-md align-middle text-center text-white"
-        @click="emits('close')">
-        &#x2715;
-      </button>
-      <div>
-        <p v-for="paragraph in paragraphs" dir="auto">{{ paragraph }}</p>
+      v-bind="$attrs"
+      class="fixed inset-0 flex justify-center items-center z-10 p-6">
+      <div class="fixed inset-0 bg-black/40 backdrop-blur-sm -z-10"></div>
+      <div class="relative w-full max-w-md">
+        <button
+          @click="emits('close')"
+          class="rounded-full w-6 h-6 bg-red-400 absolute top-0 right-0 translate-x-1/3 -translate-y-1/3 text-white">
+          x
+        </button>
+        <div
+          class="bg-white w-full max-h-72 overflow-x-hidden overflow-y-auto rounded-md px-4 pb-4 pt-6 hide-scrollbar"
+          :class="center">
+          <p v-for="paragraph in paragraphs" dir="auto">{{ paragraph }}</p>
+        </div>
       </div>
     </div>
   </Teleport>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, useAttrs } from 'vue';
 
 const props = defineProps(['text', 'center']);
 const emits = defineEmits(['close']);
+const attrs = useAttrs();
 
 const center = computed(() => ({ 'text-center': props.center }));
 const paragraphs = computed(() => props.text.split('\n'));
-console.log(props.text);
 </script>
+
+<style scoped>
+.hide-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+</style>

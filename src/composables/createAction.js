5d5bce57280192bf8a3ae5d36df821c8167315d6
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-05 19:19:24 +0430

    use translation in editAction composable

--- src/composables/createAction.js
+++ src/composables/createAction.js
@@ -1,14 +1,14 @@
-import { ref, unref } from "vue";
+import { ref, unref } from 'vue';
 
-import api from "../api";
-import { useUserStore } from "../stores/users";
-
+import api from '../api';
+import { useUserStore } from '../stores/users';
+import { useLocale } from './locale';
 
 const endpoint = '/actions';
 
 export function useCreateAction(img, title, body, notifStore) {
   const token = useUserStore().token;
-
+  const { t } = useLocale();
   const pending = ref(false);
 
   function post(cb, fcb) {
@@ -18,24 +18,26 @@ export function useCreateAction(img, title, body, notifStore) {
     const titleVal = unref(title);
     const bodyVal = unref(body);
 
-
     const formData = new FormData();
-    formData.append("avatar", imgVal);
-    formData.append("title", titleVal);
-    formData.append("body", bodyVal);
+    formData.append('avatar', imgVal);
+    formData.append('title', titleVal);
+    formData.append('body', bodyVal);
 
-    api.post(endpoint, formData, { headers: { "Content-Type": "multipart/form-data", token } })
+    api
+      .post(endpoint, formData, {
+        headers: { 'Content-Type': 'multipart/form-data', token },
+      })
       .then(res => {
-        notifStore.pushNotification("Action Created");
+        notifStore.pushNotification(t('texts.actionCreated'));
         pending.value = false;
         cb(res);
       })
       .catch(err => {
-        notifStore.pushNotification("An error occurred");
+        notifStore.pushNotification('An error occurred');
         pending.value = false;
         fcb(err);
       });
   }
 
-  return {post, pending};
+  return { post, pending };
 }
